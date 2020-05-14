5235ef0013180f39cea47d41874b237829acc443
sammyhoworth <samuelphoworth@gmail.com>
2020-05-14 14:06:51 -0400

    new challenge

--- miscellaneous/analog_hand_angle.py
+++ miscellaneous/analog_hand_angle.py
@@ -5,17 +5,18 @@ def hands_angles(t):
     m =  t.split(':')[1]
 
    
-    hang1 = 360 * ( int(h)/12 )
-    hang2 = (360/5)*(int(m)/60)
+    hang1 = 360 * ( int(h) / 12 )
+    hang2 = (360/12) * (int(m)/60)
 
     minang = 360 * ( int(m)/60 )
-    print (hang1, hang2)
+    print (hang1, hang2, ' = ', hang1+hang2)
     print(minang)
-    return 
+    return [abs((hang1+hang2) - minang), 360 - abs((hang1+hang2) - minang)]
 
 
 
 if __name__ == '__main__':
     
-    time_check = "3:20"
+    time_check = "12:01"
     angs = hands_angles(time_check)
+    print(angs)
