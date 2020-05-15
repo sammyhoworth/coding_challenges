9ba4285412c80ed8b3e16927c833eb54a5fb829f
sammyhoworth <samuelphoworth@gmail.com>
2020-05-15 00:17:08 -0400

    added solution for new year chaos

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ My solutions to select coding challenges
 1. Hackerrank  
     a. Array Manipulation   (Diffulty:   Hard)  
     b. Sparse Arrays        (Difficulty: Medium)  
+    c. New Year Chaos       (Difficulty: Medium)  
+    c. Left Rotation        (Difficulty: Easy)  
   
 2. Miscellaneous  
     a. Powerset  
---
+++ hackerrank/new_year_chaos/new_year_chaos.py
@@ -0,0 +1,44 @@
+# Problem available here: https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+# uncomment print statements for debugging
+
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumBribes function below.
+def minimumBribes(q):
+    # a person can only bribe the person in front of them (and jump them in line)
+    # a person can at most bribe two people
+    # line starts as 1 2 3 4 ... etc.
+    nmoves = 0
+    
+    for i in range(len(q)):
+        #print("i ", i)
+        if q[i] - (i+1) >= 3:
+            # if q[i] is is 3 or greater than i+1 (its 'natural' value)
+            print ("Too chaotic")
+            return
+        for j in range(max(0,q[i]-2),i):
+            #print("    checking {} against {}".format(q[j], q[i]))
+            if q[j] > q[i]:
+                nmoves += 1
+                #print("       nmoves += 1:", nmoves)
+
+    print(nmoves)
+    return 
+
+
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)
+        
