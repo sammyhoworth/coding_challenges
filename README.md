9f524935e40796327597d701c2a357bd6130094a
sammyhoworth <samuelphoworth@gmail.com>
2020-05-15 00:41:03 -0400

    minor edits

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ My solutions to select coding challenges
    . Left Rotation        (Easy)  
 
 2. Miscellaneous  
-   . Powerset  
-   . Clock hand angle  
+   . Powerset (create the powerset [set of all possible subsets] of a given set)  
+   . Clock hand angle (calculate angle between hour and minute hands for a given time)  
--- miscellaneous/clock_hand_angle.py
+++ miscellaneous/clock_hand_angle.py
@@ -3,8 +3,7 @@
 def hands_angles(t):
     h = t.split(':')[0]
     m =  t.split(':')[1]
-
-   
+  
     hang1 = 360 * ( int(h) / 12 )
     hang2 = (360/12) * (int(m)/60)
 
@@ -12,7 +11,6 @@ def hands_angles(t):
     print (hang1, hang2, ' = ', hang1+hang2)
     print(minang)
     return [abs((hang1+hang2) - minang), 360 - abs((hang1+hang2) - minang)]
-
 
 
 if __name__ == '__main__':
